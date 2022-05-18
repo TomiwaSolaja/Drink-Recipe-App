75ec447fd0cdeedbeeece7fb28d86169a9571b73
Izzi <105239770+ImIzzi@users.noreply.github.com>
2022-05-18 14:30:00 -0500

    Update db_connection.py

--- Project2BackEnd/util/db_connection.py
+++ Project2BackEnd/util/db_connection.py
@@ -6,10 +6,10 @@ def create_connection():
     try:
         conn = psycopg2.connect(
             database='mydb',
-            user='isalcedo',
-            password='Izzi90909',
-            host='database-1.crlxxrqlimxb.us-east-2.rds.amazonaws.com',
-            port='5432'
+            user='',
+            password='',
+            host='',
+            port=''
         )
 
         return conn
